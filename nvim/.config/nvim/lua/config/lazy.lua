a7eccf677c28fa6b08a7672c595bca8279f3bacc
Keven Mário <kevenmario.n.r@gmail.com>
2026-01-01 16:27:23 -0300

    feat(2026): finisher?

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -17,8 +17,29 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorschmene = "gruvbox" } },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
+
+    { import = "lazyvim.plugins.extras.lang.sql" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
+    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.util.dot" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "plugins" },
   },
   defaults = {
--- zshrc/.config/zsh/functions.zsh
+++ zshrc/.config/zsh/functions.zsh
@@ -1,2 +1,37 @@
 #!/bin/sh
 # vim: set filetype=sh :
+
+# Reset
+RESET='\033[0m'
+
+# Cores normais
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+
+# Funções para printar mensagens coloridas de forma legível
+log() {
+  printf "${CYAN}%s${RESET}\n" "$1"
+}
+
+loginfo() {
+  log "🔵 $1"
+}
+
+logsuccess() {
+  printf "🟢 ${GREEN}%s${RESET}\n" "$1"
+}
+
+logwarning() {
+  printf "🟡 ${YELLOW}%s${RESET}\n" "$1"
+}
+
+logerror() {
+  printf "🔴 ${RED}%s${RESET}\n" "$1"
+}
+
+dtouch() {
+  mkdir -p "$(dirname "$1")"
+  touch "$1"
+}
