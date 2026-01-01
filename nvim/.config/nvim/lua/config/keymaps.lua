6658733cd94507a0c00516bb8154462344d516cb
Keven Mário <kevenmario.n.r@gmail.com>
2026-01-01 15:18:17 -0300

    feat(2026): nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
 vim.g.mapleader = " "
 
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- Insert mode: jj = ESC
 map("i", "jj", "<Esc>", opts)
+map("i", "jk", "<Esc>", opts)
+
+-- -- Increment/Decrement
+-- map("n", "+", "<C-a>")
+-- map("n", "-", "<C-x>")
+--
+-- -- Delete a word backwards
+-- map("n", "dw", "vb_d")
+--
+-- -- New Tab
+-- map("n", "te", "tabedit", opts)
+-- map("n", "<tab>", ":tabnext<Return>", opts)
+-- map("n", "<s-tab>", ":tabprev<Return>", opts)
--- zshrc/.config/zsh/keybinds.zsh
+++ zshrc/.config/zsh/keybinds.zsh
@@ -1,2 +1,6 @@
 #!/bin/sh
 # vim: set filetype=sh :
+
+bindkey -e
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
