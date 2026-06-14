fa148189f61428c5b3176743d42460ca068da8d4
KevenMarioN <kevenmario.n.r@gmail.com>
2026-06-14 08:08:38 -0300

    feat(2026): polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,9 +6,9 @@ killall -q polybar
 # Aguarde até que os processos sejam encerrados
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITOR=LVDS-1 polybar example --reload 2>&1 | tee -a /tmp/polybar.log &
+MONITOR=LVDS1 polybar example --reload 2>&1 | tee -a /tmp/polybar.log &
 echo "Polybar iniciado no monitor: LVDS-1"
-MONITOR=HDMI-1 polybar example --reload 2>&1 | tee -a /tmp/polybar.log &
+MONITOR=HDMI1 polybar example --reload 2>&1 | tee -a /tmp/polybar.log &
 echo "Polybar iniciado no monitor: HDMI-1"
 
 disown
